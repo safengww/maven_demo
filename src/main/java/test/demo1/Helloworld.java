7799d6294b1d45d0283f851b14da4a987e078224
hyl <hyl@putao.cn>
2016-07-12 10:54:08 +0800

    first commit

---
+++ src/main/java/test/demo1/Helloworld.java
@@ -0,0 +1,15 @@
+package test.demo1;
+
+public class Helloworld {
+
+	public static String sayHello(){
+		return "hello world";
+	}
+	
+	
+	public static void main(String[] args) {
+		String say = sayHello();
+		System.out.println(say);
+	}
+	
+}
---
+++ src/test/java/test/demo1/HelloworldTest.java
@@ -0,0 +1,11 @@
+package test.demo1;
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+public class HelloworldTest extends TestCase{
+
+	public void testSayHello(){
+		new Helloworld().sayHello();
+	}
+	
+}
